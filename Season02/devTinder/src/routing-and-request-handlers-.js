2707babe9f3485a14f2b39ee45442a366c8c1eca
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2024-10-13 00:46:53 +0530

    moved code to separate file

---
+++ Season02/devTinder/src/routing-and-request-handlers-.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+// NOTE: ORDER OF THE ROUTES MATTER A LOT, SO KEEP THIS IN MIND WHILE DECLARING REQUEST HANDLERS
+// Exploring routehandlers & how it matches the patterns
+// will handle /xz, /xyz
+app.get("/xy?z", (req, res) => {
+    res.send("xy?z is working");
+})
+// will handle /xyz, /xyyyyyyyz
+app.get("/xy+z", (req, res) => {
+    res.send("xy+z is working");
+})
+// will handle /xyz, /xyyyyyyyz
+app.get("/xy*z", (req, res) => {
+    res.send("xy*z is working");
+})
+// will handle /xz, /xyxz
+app.get("/x(yx)?z", (req, res) => {
+    res.send("x(yx)?z is working");
+})
+
+// Exploring req.query
+app.get("/users", (req, res) => {
+    console.log("/users?userId=123&name=abc")
+    console.log(req.query)//{ userId: '123',name:'abc' }
+    res.send("Users data fetched successfully")
+})
+// Exploring req.params
+app.get("/users/:userId/:name", (req, res) => {
+    console.log("/users/:userId/:name")
+    console.log(req.params)//{ userId: '123',name:'abc' }
+    res.send("Users data fetched successfully")
+})
+
+app.post("/users", (req, res) => {
+    res.send("Users data added successfully")
+})
+
+app.put("/users", (req, res) => {
+    res.send("Users data updated successfully")
+})
+
+app.delete("/users", (req, res) => {
+    res.send("Users data deleted successfully")
+})
+
+app.use("/timeoutTest", (req, res) => {
+    console.log("It will keep waiting for response from server")
+    // As there is no res returned here, it will be waiting and eventually will be timedout
+})
+
+// Always keep / path at the end OR request will be always handled by this handler
+app.use(
+    "/", // PATH
+    // Request handler
+    (req, res) => {
+        res.send("Welcome to devTinder server, No such route")
+    }
+)
+app.listen(7777, () => {
+    console.log("Running server on port 7777")
+})
