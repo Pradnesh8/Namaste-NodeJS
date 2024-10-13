d9f24ec1254dcae94a5f4c5b2553ca38f676d252
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2024-10-13 21:34:19 +0530

    - middleware for authhandler - How & why middleware

--- Season02/devTinder/src/app.js
+++ Season02/devTinder/src/app.js
@@ -1,7 +1,33 @@
 const express = require('express')
+const { adminAuth, userAuth } = require('./middlewares/authHandler')
 const app = express()
 
-app.use("/user",
+// HOW EXPRESS WORKS
+// /route => Middleware chain (Functions/ logic will be called and then moved to next ) => Request handler (resolve the request with response)
+
+// This middleware will handle all /admin request GET, POST, ....,etc. 
+// If unauthorized, return error with 401 status
+// if authorized, will move to request handler
+app.use('/admin', adminAuth);
+
+app.get("/admin/getAllUsers", (req, res) => {
+    res.send("ALL USER DATA")
+})
+
+app.delete("/admin/deleteUser", (req, res) => {
+    res.send("USER DELETED")
+})
+
+// HERE as user needs to login, we wont check the user Auth 
+app.post("/user/login", (req, res, next) => {
+    res.send("LOG IN")
+})
+// Here as user is trying to access the profile data, we must check the authrozation of user
+app.get("/user/profile", userAuth, (req, res, next) => {
+    res.send("USER PROFILE DATA")
+})
+
+app.use("/user2",
     // we can use N no. of middlewares / request handlers
     (req, res, next) => {
         console.log("Req handler1 called")
