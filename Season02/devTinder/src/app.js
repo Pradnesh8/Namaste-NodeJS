bdae8320b56d714cd619b6e3adba6d6a745b083e
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2024-10-15 00:42:53 +0530

    signup API, to add / save user in mongodb

--- Season02/devTinder/src/app.js
+++ Season02/devTinder/src/app.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const connectDB = require('./config/database')
+const User = require("./models/user")
 const app = express()
+
+app.post("/signup", async (req, res) => {
+    const userObj = {
+        firstName: "John",
+        lastName: "Doe",
+        emailId: "john.doe@gmail.com",
+        password: "john@123",
+        age: 28,
+        gender: 'Male',
+    }
+    try {
+        const user = new User(userObj);
+        await user.save()
+        res.send("User added successfully")
+    } catch (err) {
+        res.status(400).send("Error adding user, err: ", err.message)
+    }
+})
 
 connectDB().then(() => {
     console.log("Successfully connected to DB")
