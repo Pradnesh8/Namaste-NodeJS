2c85b5b1e0a16f7e0eab780931d01ede46c15a34
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2024-11-05 00:34:25 +0530

    API for feed, extracts all the users which are not connections, ignored, or self

--- Season02/devTinder/src/routes/user.js
+++ Season02/devTinder/src/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const userRouter = express.Router()
 const { userAuth } = require("../middlewares/authHandler");
 const { ConnectionRequest } = require('../models/connectionRequest');
+const User = require('../models/user');
 
 
 const SAFE_USER_FIELDS = ["firstName", "lastName", "photo_url", "skill", "about", "age", "gender"]
@@ -51,6 +52,41 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         })
     } catch (err) {
         res.status(400).send("ERROR: " + err.message);
+    }
+})
+
+// GET Feed
+userRouter.get("/feed", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+        // Show all the users from DB except
+        // 0. logged in user profile
+        // 1. users from which logged in user request/received connection
+        // 2. users which are already connections
+
+        const connectionRequests = await ConnectionRequest.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ]
+        }).select("fromUserId toUserId");
+
+        const hideUserProfiles = new Set()
+        connectionRequests.forEach(request => {
+            hideUserProfiles.add(request.fromUserId)
+            hideUserProfiles.add(request.toUserId)
+        });
+
+        const users = await User.find({
+            $and: [
+                { _id: { $nin: Array.from(hideUserProfiles) } },
+                { _id: { $ne: loggedInUser._id } }
+            ]
+        }).select(SAFE_USER_FIELDS.join(" "))
+
+        res.send(users)
+    } catch (err) {
+        res.status(400).send("ERROR: " + err.message)
     }
 })
 
