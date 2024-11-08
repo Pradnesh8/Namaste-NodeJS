4c734cd97d306dc6696bdd13ff8c5bc58bbdf01a
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2024-11-09 00:18:52 +0530

    configured routes and child routes

--- Season02/devTinder-ui/src/App.jsx
+++ Season02/devTinder-ui/src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import NavBar from "./NavBar"
+import Login from "./Login"
+import Profile from "./Profile"
+import Body from "./Body"
 
 function App() {
 
   return (
     <>
-      <NavBar />
-      <h1>DevTinder</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
