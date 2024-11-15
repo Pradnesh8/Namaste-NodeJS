79c179568377aca62aff20b5d62ea48115a53cc1
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2024-11-16 00:54:25 +0530

    configured redux store Created userSlice provided store to app

--- Season02/devTinder-ui/src/App.jsx
+++ Season02/devTinder-ui/src/App.jsx
@@ -1,21 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import NavBar from "./NavBar"
+
 import Login from "./Login"
 import Profile from "./Profile"
 import Body from "./Body"
+import { Provider } from "react-redux"
+import appStore from "./utils/appStore"
+import Feed from "./Feed"
 
 function App() {
 
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/" element={<Feed />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
