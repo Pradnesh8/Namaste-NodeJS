98db4168827170d3fb0f29138a580c464764a6f9
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2024-11-17 23:51:41 +0530

    /profile navigation added

--- Season02/devTinder-ui/src/components/NavBar.jsx
+++ Season02/devTinder-ui/src/components/NavBar.jsx
@@ -49,7 +49,7 @@ const NavBar = () => {
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <li>
+                            <li onClick={() => navigate("/profile")}>
                                 <a className="justify-between">
                                     Profile
                                     <span className="badge">New</span>
