92c1826178d1497cf1649eb69e0b6a71b1fc84b4
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2024-11-22 00:54:59 +0530

    sign up feature, with toggle

--- Season02/devTinder-ui/src/components/Login.jsx
+++ Season02/devTinder-ui/src/components/Login.jsx
@@ -7,9 +7,12 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const user = useSelector(store => store.user)
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
     const [emailId, setEmailId] = useState("")
     const [password, setPassword] = useState("")
     const [errMsg, setErrMsg] = useState("")
+    const [isLoginForm, setIsLoginForm] = useState(true)
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const submitHandler = async () => {
@@ -25,9 +28,24 @@ const Login = () => {
             console.error(err)
         }
     }
+    const signUpHandler = async () => {
+        try {
+            const res = await axios.post(BASE_URL + "/signup", {
+                firstName, lastName, emailId, password
+            }, { withCredentials: true })
+            console.log("Sign up Successful");
+            dispatch(addUser(res?.data?.data));
+            navigate("/profile")
+        } catch (err) {
+            setErrMsg(err?.response?.data || "Something went wrong")
+            console.error(err)
+        }
+    }
     const checkIfLoggedIn = () => {
         if (user) {
-            navigate("/")
+            isLoginForm ?
+                navigate("/") :
+                navigate("/profile")
         }
     }
     useEffect(() => {
@@ -37,7 +55,26 @@ const Login = () => {
         <div className='flex justify-center my-32'>
             <div className="card bg-base-300 w-96 shadow-xl">
                 <div className="card-body">
-                    <h2 className="card-title justify-center">Login</h2>
+                    <h2 className="card-title justify-center">{isLoginForm ? "Login" : "Sign Up"}</h2>
+                    {
+                        !isLoginForm &&
+                        (
+                            <>
+                                <label className="form-control w-full max-w-xs">
+                                    <div className="label">
+                                        <span className="label-text">First Name</span>
+                                    </div>
+                                    <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} className="input input-bordered w-full max-w-xs" />
+                                </label>
+                                <label className="form-control w-full max-w-xs">
+                                    <div className="label">
+                                        <span className="label-text">Last Name</span>
+                                    </div>
+                                    <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} className="input input-bordered w-full max-w-xs" />
+                                </label>
+                            </>
+                        )
+                    }
                     <label className="form-control w-full max-w-xs">
                         <div className="label">
                             <span className="label-text">Email ID</span>
@@ -52,8 +89,15 @@ const Login = () => {
                     </label>
                     <div className='text-error'>{errMsg}</div>
                     <div className="card-actions justify-center mt-4">
-                        <button className="btn btn-primary w-1/3" onClick={submitHandler}>Submit</button>
+                        <button className="btn btn-primary w-1/3" onClick={isLoginForm ? submitHandler : signUpHandler}>Submit</button>
                     </div>
+                    <p className="text-center cursor-pointer my-2" onClick={() => setIsLoginForm(!isLoginForm)}>
+                        {
+                            isLoginForm ?
+                                "New user? Sign up here" :
+                                "Already an user? Login here"
+                        }
+                    </p>
                 </div>
             </div>
         </div>
