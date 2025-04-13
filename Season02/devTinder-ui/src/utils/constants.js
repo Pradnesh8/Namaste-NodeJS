3fb2e8d816d3923dab4b272ed3d08efd732f72ab
Pradnesh Khedekar <pradneshkhedekar08@gmail.com>
2025-04-13 19:54:53 +0530

    use /api on prod as base_url

--- Season02/devTinder-ui/src/utils/constants.js
+++ Season02/devTinder-ui/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:7777"
+export const BASE_URL = location.host === 'localhost' ? "http://localhost:7777" : "/api"
 export const DEFAULT_PROFILE_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA6g9BWr61gs6KYIq3zjFEy36Z8OuOIJQ75A&s"
